4cb997def48fb4633017610310296992e110328c
use-mrshelby <app@mrshelby.com.br>
2026-04-27 20:58:42 -0300

    Update jadlog.js

--- api/jadlog.js
+++ api/jadlog.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
       {
         method: 'POST',
         headers: {
-          'Authorization': token,
+          'Authorization': `Bearer ${token}`,
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ consulta: [{ [campo]: codigo }] })
