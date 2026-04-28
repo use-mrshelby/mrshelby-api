7c9a650aada6db3c78c9c2391f39a02d07bdb5a2
use-mrshelby <app@mrshelby.com.br>
2026-04-28 16:58:47 -0300

    Update correios.js

--- api/correios.js
+++ api/correios.js
@@ -1,3 +1,4 @@
+// deploy 28/04
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
